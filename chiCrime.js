9ee58ff29442391af93a4b00634df8d51b840f57
Julia Sharp <juliasharp96@gmail.com>
2016-12-07 13:46:17 -0800

    Update chiCrime.js

--- chiCrime.js
+++ chiCrime.js
@@ -1,5 +1,9 @@
-var width = 1000,
+var width = 700,
     height = 800;
+
+var margin = {top: 500, right: 40, bottom: 0, left: 100}
+    graph_width = 500 - margin.left - margin.right,
+    graph_height = 900 - margin.top - margin.bottom
 
 var formatNumber = d3.format(",d");
 
@@ -9,39 +13,59 @@ var tipDetail = {population:"population", crime:"crimePerK"},select;
 //define color scale showing crime
 var color = d3.scale.threshold()
     .domain([0, 10, 20, 30, 40, 50, 60])
-    .range(colorbrewer.Blues[8]);
+    .range(colorbrewer.Reds[8]);
+
+var graph_color = d3.scale.ordinal()
+    .range(["#98abc5", "#6b486b", "#d0743c"])
 
 //position encoding for the key only.
 var x = d3.scale.linear()
-    //.domain([0, 5100]) //-0.6, 5
     .domain([-0.6, 65])
     .range([0, 480]);
 
 //Define x-axis
-var xAxis = d3.svg.axis()
+var legend_xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom")
     .tickSize(13)
     .tickValues(color.domain())
     .tickFormat(function(d){ return (d)})
-    //.tickFormat(function(d) { return d >= 100 ? formatNumber(d) : null; });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
+//an svg to append both svg's
+var parentSVG = d3.select("body")
+    .attr("align","center")
+    .append("svg")
+    .attr("id","parentSVG")
+    .attr("width", width*2)
+    .attr("height", height);
+
+//svg for map
+var svg = d3.select("#parentSVG")
+    .append("svg")
+    .attr("width", (width-100))
+    .attr("height", height)
+    .on("click", reset)
+
+var g = svg.append("g")
+
+//svg for bar graph
+var svg2 = d3.select("#parentSVG")
+    .append("svg")
+    .attr("x",width-100)
+    .attr("width", width-100)
     .attr("height", height);
 
 //space for tooltip
 var tooltip = d3.select("body").append("div")
     .attr("class","tooltip")
     .style("position", "absolute")
-    //.style("opacity",0)
     .style("display", "none");
 
 
 //Draw Legend
 var legend = svg.append("g")
     .attr("class", "key")
-    .attr("transform", "translate(600,20)");
+    .attr("transform", "translate(20,700)");
 
 //color values for legend
 legend.selectAll("rect")
@@ -58,13 +82,27 @@ legend.selectAll("rect")
     .attr("width", function(d) { return d.x1 - d.x0; })
     .style("fill", function(d) { return d.z; });
 
-legend.call(xAxis).append("text")
+legend.call(legend_xAxis).append("text")
     .attr("class", "caption")
     .attr("y", -6)
     .text("Crime per 1000 Residents");
 
+var graph_xScale = d3.scale.ordinal().rangeRoundBands([0,300], 0.1)
+var graph_yScale = d3.scale.linear().range([graph_height, 0])
+
+var graph_xAxis = d3.svg.axis()
+  .scale(graph_xScale)
+  .orient("bottom");
+
+var graph_yAxis = d3.svg.axis()
+  .scale(graph_yScale)
+  .orient("left")
+  .ticks(5)
+
 //globals
 var popRange, crimeRange;
+
+var active;
 var Cdata;
 
 //Load JSON file
@@ -77,7 +115,6 @@ d3.json("ChicagoData.json", function(error, json) {
    //get data from file
    var features = topojson.feature(json, json.objects.features);
 
-   //make features global
    Cdata = features;
 
    //correctly centers map
@@ -86,7 +123,7 @@ d3.json("ChicagoData.json", function(error, json) {
       .parallels([40, 45])
       .scale(100000)
       .rotate([92.35, .5, -4])
-      .translate([((width / 2)-150), ((height / 2)-50)]);
+      .translate([((width / 2)-70), ((height / 2)-50)]);
 
    //calls the projection
    var path = d3.geo.path().projection(projection);
@@ -97,63 +134,161 @@ d3.json("ChicagoData.json", function(error, json) {
        .enter().append("path")
        .attr("class", "neighborhood")
        .attr("d", path)
-       //color map according to crime per capita
-       .style("fill",
-       function(d) {
-        return color(d.properties["crimePerK"]);
+       .style("fill", function(d) {
+          return color(d.properties["crimePerK"]);
       })
-       //tooltip and dim on mouseover
        .on("mouseover", function(d){
-          //dimMap(Cdata, svg, true)
+          dimMap(Cdata, svg, d.properties["comName"], true)
           tooltip.transition()
                  .duration(200)
                  .style("opacity", .9)
                  TooltipText(d,"comName","population","crimePerK", "percentbelowpoverty")
                     return tooltip.style("display","inline");
         })
-    //place toolTip in one location
     .on("mousemove", function(d){
-        return tooltip.style("top", (height-420)+"px").style("left",(width-960)+"px");
+        return tooltip.style("top", (height-340)+"px").style("left",(width-690)+"px");
     })
-    //toolTip fade out
     .on("mouseout", function(d){
-        //dimMap(Cdata, svg, false)
+        //dimMap(Cdata, svg, d.properties["comName"], false)
         tooltip.transition()
         .duration(500)
         .style("opacity", 0);
     })
-    //on click, show more info about community selected
-    .on("click", function(d){
-      //clearLabel(d)
-      console.log(d.properties["comName"])
-      communityLabel(d)
-      //show bar chart
-    })
+    .on("click", click)
 
 });
 
+function click(d){
+
+  g.selectAll(".Cdata")
+    .style("fill", function(d){console.log("init");return color(d.properties["population"])})
+
+  if (active === d) return reset();
+    g.selectAll(".active")
+     .classed("active", false);
+    d3.select(this).classed("active", active = d);
+
+  clearLabel(d);
+  communityLabel(d);
+  appendLegend(Cdata, svg2);
+  makeGraph(Cdata, svg2, d.properties["violentCrimes"], d.properties["homocideAssault"], d.properties["firearmRelated"])
+}
+
+function reset(){
+
+  g.selectAll(".active").classed("active", active = false);
+
+}
+
+function makeGraph(data, graph, violent, homocide, fireArm){
+
+  var value = [{name:'Violent', number: violent}, {name:'Homicide', number: homocide}, {name:'FireArm', number: fireArm}];
+
+  //returns range each axis--> x-axis = community, y-axis = percent below poverty
+  graph_xScale.domain(value.map(function(d){return d.name;}));
+  graph_yScale.domain([0,  value[0].number])
+
+  //draws x-axis and positions label
+  /*
+  graph.append("g")
+     .attr("class", "axis")
+     //.attr("transform", "translate(80, 500)")
+     .attr("transform", "translate(80," + (graph_height) + ")")
+     .call(graph_xAxis)
+     //.selectAll("text")
+     //.attr("dx", "-.8em")
+     //.attr("dy", ".25em")
+     //.style("text-anchor", "end")
+     //.attr("font-size", "10px")
+    */
+
+  //draws y-axis and positions label
+  graph.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(80," + (0) + ")")
+        .call(graph_yAxis)
+        .selectAll("text")
+        .attr("dx", "-.8em")
+        .attr("dy", ".25em")
+        .style("text-anchor", "end")
+        .attr("font-size", "10px")
+
+
+  graph.selectAll('.bar')
+     .data(value)
+     .enter()
+     .append('rect')
+     .attr("classed", "bar")
+     .attr("class","bars")
+     .attr("height", 0)
+     .attr("transform", "translate(80, 0)")
+     .transition()
+     .duration(700)
+     .ease("linear")
+     .attr({
+        "x": function(d) {return graph_xScale(d.name); },
+        "y": function(d){return graph_yScale(d.number); },
+        "width": "50px",
+        "height":  function(d){return graph_height - graph_yScale(d.number); },
+        "fill": function(d){ return (graph_color(d.name))}
+      })
+}
+
+function appendLegend(data, legend){
+
+  var labels = ["Violent Crimes", "Homocide Assualt", "Fire Arm Related"]
+
+  var graph_legend = legend.selectAll(".legend")
+      .data(labels)
+      .enter().append("g")
+      .attr("class", "graph_legend")
+      .attr("transform", "translate(200, 600)")
+      .attr("transform", function(d, i) { return "translate(0," + i * 40 + ")"; });
+
+  graph_legend.append("rect")
+    .attr("x", graph_width - 18)
+    .attr("width", 36)
+    .attr("height", 36)
+    .attr("fill", graph_color)
+
+  graph_legend.append("text")
+      .attr("x", graph_width - 30)
+      .attr("y", 20)
+      .attr("class", "legend")
+      .attr("dy", ".7em")
+      .attr("font-size", "12px")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+}
+
 function clearLabel(d, name){
-  svg.remove("text")
-      .text(d.properties["comName"])
-      .attr("x", 700)
-      .attr("y", 100)
-      .text(d.properties["comName"])
+  svg2.selectAll("text").remove()
+      .attr("x", 200)
+      .attr("y", 200)
+      .attr("id", "neighborhood-label")
+      .text(" ")
+
+  svg2.selectAll('bar').data([]).exit().remove()
+      .attr("classed", "bar")
+      .attr("class", "bars")
+
 }
 
 function communityLabel(d, name){
-  svg.append("text")
-     .attr("x", 800)
-     .attr("y", 100)
+  svg2.append("text")
+     .attr("x", 200)
+     .attr("y", 200)
      .attr("id", "neighborhood-label" )
      .text(d.properties["comName"])
      .style("opacity", 1)
-
 }
 
-function dimMap(data, map, dim){
-      map.selectAll("path")
-         .data(data.features)
-         //.transition().duration(1000)
+function dimMap(data, map, name, dim){
+     var selected = [name]
+
+      map.selectAll("data")
+         .data(selected)
+         .transition().duration(1000)
          .style("opacity", function(d){
             if(dim){
               return "0.5";
@@ -164,9 +299,9 @@ function dimMap(data, map, dim){
 //Define the conent of the ToolTip
 function TooltipText(d,name,pop,crime, poverty){
     tooltip.html("<center><b>"+d.properties[name]+"</b></center><br/>"
-                        +"Population : " + d.properties[pop]+" people</em>"+"<br/>"
-                        +"Households below poverty level: " + d.properties[poverty] + "%</em>" + "<br/>"
-                        +"Crime: "+d.properties[crime]+"</em><br/>"
+                        +"Population <hideText> __di ___</hideText> :" + "<em>"  + d.properties[pop]+ "</em><br/>"
+                        +"Below Poverty Level: " + "<em> " + "<hideText>_____</hideText>" + d.properties[poverty] + "</em>" + "<br/>"
+                        +"Crime <hideText> ___________</hideText>: " + "<em>" + "<hideText>_____</hideText>" + d.properties[crime] + "</em><br/>"
                         +'<div id="help">*Crime Rate Per 1000 Residents<div>'
                         );
 }
